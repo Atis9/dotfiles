1646930a0a61dfb9fdf70711288de7d74fa14a97
Atis <atis@nyantech.com>
2024-07-31 02:09:07 +0900

    Revert "Refactor nvim config"
    This reverts commit 753cfbb3fab65890fdd0e69af0759302acf87b13.

--- config/nvim/lua/plugins/keymaps.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR><Esc>")
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
-
--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -7,9 +7,6 @@ return {
   },
   config = function()
     require("nvim-tree").setup {}
-
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
   end,
 }
 
