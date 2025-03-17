c6a27dfed13a9a41d17a16827689b4486e0517e9
Atis <atis@nyantech.com>
2025-03-17 17:49:11 +0900

    Update `reset-launchpad` to using cache purging

--- config/zsh/plugins/alias.zsh
+++ config/zsh/plugins/alias.zsh
@@ -8,7 +8,7 @@ alias dc='docker compose'
 
 if [ `uname` = "Darwin" ]; then
   alias ls='ls -G'
-  alias reset-launchpad='defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock'
+  alias reset-launchpad='sudo find 2>/dev/null /private/var/folders/ -type d -name com.apple.dock.launchpad -exec rm -rf {} +; killall Dock'
 else
   alias ls='ls --color=auto'
 fi
