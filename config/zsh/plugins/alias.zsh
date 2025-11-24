8facff98912c2dfaee0e6232acfb5448ad1e6bcf
Atis <atis@nyantech.com>
2025-11-24 16:01:33 +0900

    Add alias `half2full`

--- config/zsh/plugins/alias.zsh
+++ config/zsh/plugins/alias.zsh
@@ -5,6 +5,7 @@ alias ll='ls -lh'
 alias la='ls -lha'
 alias k='kubectl'
 alias dc='docker compose'
+alias half2full='ruby -pe '\''$_.tr!("!-~", "！-～")'\'''
 
 if [[ $(uname) = 'Darwin' ]]; then
   alias ls='ls -G'
