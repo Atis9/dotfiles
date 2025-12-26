ae25c4b12eb048caf4d03aa6d0b24ed679f0dac9
Atis <atis@nyantech.com>
2025-12-27 02:44:12 +0900

    Fix brew PATH

--- config/zsh/plugins/brew.zsh
+++ config/zsh/plugins/brew.zsh
@@ -1,3 +1,6 @@
-if [[ $(uname) = 'Darwin' ]]; then
+if [[ -x /opt/homebrew/bin/brew ]]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
